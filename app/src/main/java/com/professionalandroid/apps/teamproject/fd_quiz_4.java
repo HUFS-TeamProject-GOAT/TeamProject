212a3f7bd6bd3d337659e2cd7aa4e2ecf249e9e6
multiful <rlaehdrb2485@naver.com>
2023-06-07 09:25:46 +0900

    quiz add

---
+++ app/src/main/java/com/professionalandroid/apps/teamproject/fd_quiz_4.java
@@ -0,0 +1,14 @@
+package com.professionalandroid.apps.teamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fd_quiz_4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fd_quiz4);
+    }
+}
