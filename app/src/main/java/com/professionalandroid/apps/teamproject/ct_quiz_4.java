b804aa9759dc28ebbc85c68b117354b47cdabbe9
yoontaemin726 <2003taemin@gmail.com>
2023-06-19 19:17:50 +0900

    최종 -0.4

--- app/src/main/java/com/professionalandroid/apps/teamproject/ct_quiz_4.java
+++ app/src/main/java/com/professionalandroid/apps/teamproject/ct_quiz_4.java
@@ -44,6 +44,8 @@ public class ct_quiz_4 extends AppCompatActivity {
         score = sharedPreferences.getInt(SCORE_KEY, SCORE_DEFAULT);
         count = sharedPreferences.getInt(COUNT_KEY, COUNT_DEFAULT);
 
+
+
         System.out.println("Saved Score: " + score);
         System.out.println("Saved Count: " + count);
         updateScoreText();
